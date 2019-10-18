86df8901f31304334dc6bf03f62de95b8172e75f
Emmanuel Darko <mannyampemdarko@gmail.com>
2019-10-17 22:49:11 -0500

    Adding files

---
+++ README.md
@@ -0,0 +1,15 @@
+# Surfing in Costa Rica
+
+* The purpose of this project was to create a web page the will display the weather in Costa Rica as well as the time and date using Python and Flask.
+
+* We used "surfline" (https://www.surfline.com/surf-reports-forecasts-cams/costa-rica/3624060) grab the names of location as well as the height of the surf. For example "Camaronal has 6-7 FT surf"
+
+It entails:
+
+* A `app.py` file serves as a router and will use a python `scrape_weather.py` file to scrape the data.
+
+* Storage of every scrape into a database.
+
+* The webpage will display all the results from the database.
+
+
---
+++ app.py
@@ -0,0 +1,50 @@
+# import necessary libraries
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_info
+
+# create instance of Flask app
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+mongo = PyMongo(app)
+
+
+# create route that renders index.html template and finds documents from mongo
+@app.route("/")
+def home():
+
+    # Find data
+    forecasts = mongo.db.collection.find()
+
+    # return template and data
+    return render_template("index.html", forecasts=forecasts)
+
+
+# Route that will trigger scrape functions
+@app.route("/scrape")
+def scrape():
+
+    # Run scraped functions
+    weather = scrape_info.scrape_weather()
+    surf = scrape_info.scrape_surf()
+
+    # Store results into a dictionary
+    forecast = {
+        "time": weather["time"],
+        "location": weather["name"],
+        "min_temp": weather["min_temp"],
+        "max_temp": weather["max_temp"],
+        "surf_location": surf["location"],
+        "height": surf["height"]
+    }
+
+    # Insert forecast into database
+    mongo.db.collection.insert_one(forecast)
+
+    # Redirect back to home page
+    return redirect("http://localhost:5000/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
